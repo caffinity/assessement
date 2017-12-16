6c58343fd97b1e97a12e4dc82a07acea4cf7eda2
Alexander Cohen <Alexcohen.is@gmail.com>
2017-12-16 22:07:32 +0000

    Included Semantic ui CDN"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216152823) do
+ActiveRecord::Schema.define(version: 20171216212446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20171216152823) do
   create_table "classrooms", force: :cascade do |t|
     t.string "name"
     t.integer "teacher_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "proficiency_levels", force: :cascade do |t|
