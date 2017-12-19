27e7ce0bafcf62eb8cd22874e288db2eee9cc58d
Alexander Cohen <Alexcohen.is@gmail.com>
2017-12-19 10:11:37 +0000

    added class room

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,45 @@
 class StudentsController < ApplicationController
-
-  def index
-
-  	@students = Student.all
-
-  end
+	before_action :authenticate_teacher!
 
 
-	  
+
+	def index
+		@students =	current_teacher.admin? ? Student.all : current_teacher.classroom
+	end
+
+	def new	
+		@students = Student.new
+	end
+
+	def show
+	end
+
+
+	def create
+		@students = Student.new(params(student_params))
+
+		if @student.save?
+			redirect_to student_path
+		else
+			render 'show'
+		end
+	end
+
+
+
+	  private
+
+	  def student_params
+	  	params.require(:students).permit(	:first_name, 
+	  																		:last_name, 
+	  																		:special_needs_education,
+	  																		:classroom_id,
+	  																		:year,
+	  																		:pupil_premium,
+	  																		:english_as_language,
+	  																		:gender
+	  																	)
+	  end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,12 @@
 teacher = FactoryBot.create(:teacher)
 classroom = FactoryBot.create(:classroom, teacher: teacher)
 student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+student = FactoryBot.create(:student, classroom: classroom)
+
