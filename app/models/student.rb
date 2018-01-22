7c52078623308dd0f779f417e267e85da0bd1d0e
Alexander Cohen <Alexcohen.is@gmail.com>
2018-01-22 17:21:32 +0000

    updated students model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ApplicationRecord
   has_many :skill_levels
   belongs_to :classroom
+  accepts_nested_attributes_for :skill_levels #proficiency_level_id & #student_id Statement optional
 end
