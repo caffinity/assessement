2ac2e8fd10b43f40b7cb6ad5a667b24f902ad1d3
Alexander Cohen <Alexcohen.is@gmail.com>
2018-01-23 22:07:19 +0000

    Updated Proficiency Level to include a /new

--- app/controllers/proficiency_levels_controller.rb
+++ app/controllers/proficiency_levels_controller.rb
@@ -2,7 +2,7 @@ class ProficiencyLevelsController < ApplicationController
 
   def index
 
-  end 
+  end
 
   def show
     @proficiency_level = ProficiencyLevel.find(params[:id])
@@ -12,18 +12,18 @@ class ProficiencyLevelsController < ApplicationController
 
 
   def new
-    @skill_level = SkillLevel.find(params[:skilllevel_id])
     @proficiency_level = ProficiencyLevel.new()
+    @skill = Skill.find_by(id: params[:skill_reference])
   end
 
   def create
-    @skill_level = SkillLevel.find(params[:skilllevel_id])
     @proficiency_level = ProficiencyLevel.new(proficiency_level_params)
+    @skill = Skill.find_by(id: params[:skill_reference])
 
     if @proficiency_level.save
-      redirect_to proficiencylevel_path(@proficiency_level)
+      redirect_to skill_path(@proficiency_level.skill_id)
     else
-
+      render 'index'
     end
   end
 
@@ -49,7 +49,7 @@ class ProficiencyLevelsController < ApplicationController
   private
 
   def proficiency_level_params
-    params.require(:proficiency_level).permit(:id, :level, :statement, :skill_level_id)
+    params.require(:proficiency_level).permit(:id, :level, :statement, :skill_id)
   end
 
 end
