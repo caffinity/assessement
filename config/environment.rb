20c4426f9e48da4febde876948d9a685f53a3dc0
Alexander Cohen <Alexcohen.is@gmail.com>
2018-01-31 14:56:51 +0000

    Configured the local and production active mailer.

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,13 @@
 require_relative 'application'
 require 'carrierwave/orm/activerecord'
 # Initialize the Rails application.
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'SENDGRID_USERNAME',
+  :password => 'SENDGRID_PASSWORD',
+  :domain => 'pointedapp.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 Rails.application.initialize!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,5 +51,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password =>  ENV["SENDGRID_PASSWORD"],
+    :domain => 'pointedapp.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
