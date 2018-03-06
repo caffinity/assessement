f059abc32c3ef82eacdd602bde2426915bb56489
Alexander Cohen <Alexcohen.is@gmail.com>
2018-03-06 13:13:33 +0000

    Added validation to accept only approved emails.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,6 +6,14 @@ class Teacher < ApplicationRecord
 
   has_one :classroom
 
-  validates :email, format: { with: /@pentlandfields.com/i,
-message: "You must register for this site with an @pentlandfields.com email address" }
+  APPROVED_DOMAINS = ["lgflmail.org", "pentlandfieldschool.co.uk", "theedenacademy.co.uk"]
+
+  validates :email, presence: true, if: :domain_check
+
+  def domain_check
+    unless APPROVED_DOMAINS.any? { |word| email.end_with?(word) }
+      errors.add(:email, ": You must register for this site with an authorised email domain. For more information please see your administrator")
+    end
+  end
+
 end
