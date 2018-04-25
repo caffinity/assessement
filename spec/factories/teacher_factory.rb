6bea8aa0e696ccdf33547c603328684e502cc520
Alexander Cohen <Alexcohen.is@gmail.com>
2018-04-25 10:12:25 +0100

    made some fixes

--- spec/factories/teacher_factory.rb
+++ spec/factories/teacher_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :teacher do
     first_name  { Faker::Name.first_name }
     last_name   { Faker::Name.last_name  }
-    email       { Faker::Internet.email  }
+    email       { "example@pentlandfieldschool.co.uk"  }
     password              '1234567890'
     password_confirmation '1234567890'
   end
